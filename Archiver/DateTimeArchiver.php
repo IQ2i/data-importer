1e0fdb407d596a0c7d5c574b8224316b2913e6f9
Loïc Sapone <loic@sapone.fr>
2020-10-16 11:43:19 +0200

    Change email address

--- Archiver/DateTimeArchiver.php
+++ Archiver/DateTimeArchiver.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the DataImporter package.
  *
- * (c) Loïc Sapone <contact@iq2i.com>
+ * (c) Loïc Sapone <loic@sapone.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
