4aee8fb644640d3d6fc43c2efd5acb6f14713830
Loïc Sapone <loic@sapone.fr>
2023-04-10 17:49:18 +0200

    Upgrade to PHPUnit 10

--- rector.php
+++ rector.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -26,6 +27,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         LevelSetList::UP_TO_PHP_81,
+        PHPUnitSetList::PHPUNIT_100,
     ]);
 
     $rectorConfig->skip([
