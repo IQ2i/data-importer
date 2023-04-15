0d51d682a045e8d8d58633b8c270827957dc7f5c
Loïc Sapone <loic@sapone.fr>
2023-04-15 20:39:53 +0200

    Add an option to AbstractImportCommand to use async mode

---
+++ src/Bundle/Processor/AsyncCliProcessor.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the DataImporter package.
+ *
+ * (c) Loïc Sapone <loic@sapone.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IQ2i\DataImporter\Bundle\Processor;
+
+use IQ2i\DataImporter\Processor\AsyncProcessorInterface;
+
+class AsyncCliProcessor extends CliProcessor implements AsyncProcessorInterface
+{
+}
