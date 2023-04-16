49543123e9c6a456ace4f5ae8bc96d66e6cff6d4
Loïc Sapone <loic@sapone.fr>
2023-04-16 17:14:37 +0200

    Create Symfony bundle inside DataImporter package
    bundle

---
+++ src/Bundle/Iq2iDataImporterBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the DataImporter package.
+ *
+ * (c) Loïc Sapone <loic@sapone.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IQ2i\DataImporter\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Iq2iDataImporterBundle extends Bundle
+{
+}
