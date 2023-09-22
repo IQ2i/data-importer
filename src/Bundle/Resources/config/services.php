7a35c64f800314f1d98768af28e0bb61f41d6f7a
Loïc Sapone <loic@sapone.fr>
2023-09-22 14:31:52 +0200

    Manage command for Symfony app with default path and namespace

--- src/Bundle/Resources/config/services.php
+++ src/Bundle/Resources/config/services.php
@@ -14,9 +14,15 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use IQ2i\DataImporter\Bundle\Messenger\MessageHandler;
+use IQ2i\DataImporter\Command\GenerateDtoCommand;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
+        ->set('iq2i_data_importer.generate_dto_command', GenerateDtoCommand::class)
+            ->arg('$defaultPath', '%kernel.project_dir%')
+            ->arg('$defaultNamespace', 'App\\Dto')
+            ->tag('console.command', ['command' => 'iq2i:data-importer:generate-dto'])
+
         ->set('iq2i_data_importer.messenger.message_handler', MessageHandler::class)
             ->tag('messenger.message_handler')
     ;
